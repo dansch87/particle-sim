a931118e7fd9e849ae08b27b5354c2011cd92d32
Daniel Schulz <daniel.schulz.lg@gmail.com>
2024-08-13 20:35:17 +0200

    Update build script for new particle files and new /src directory

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
 set -xe
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
-clang `pkg-config --cflags raylib` -o main main.c `pkg-config --libs raylib`
+
+clang `pkg-config --cflags raylib` -c src/particle.c -o build/particle.o `pkg-config --libs raylib`
+clang `pkg-config --cflags raylib` -c src/main.c -o build/main.o `pkg-config --libs raylib`
+clang build/main.o build/particle.o -o build/particle_simulation -lraylib -lm
+
+./build/particle_simulation
